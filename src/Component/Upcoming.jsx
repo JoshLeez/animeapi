39bc4f203ffc75805b94061d52f27e85f54ef151
JoshLeez <109338498+JoshLeez@users.noreply.github.com>
2022-09-28 16:03:05 +0700

    Update Upcoming.jsx

--- src/Component/Upcoming.jsx
+++ src/Component/Upcoming.jsx
@@ -12,7 +12,11 @@ function Upcoming() {
   const loadData = async() =>{
       const anime = await fetch(`https://api.jikan.moe/v4/seasons/upcoming`);
       const value = await anime.json()
-      setData(value.data)
+       if (anime.ok) {
+        setTimeout(() => {
+          return    setData(value.data);
+        }, 3000);
+      }
   }
 
   useEffect(() =>{
